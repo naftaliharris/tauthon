f9001c0bb10d509212f7c9ff459934c5727ee9fe
Yury Selivanov <yselivanov@sprymix.com>
2015-05-28 11:21:31 -0400

    Issue 24017: Drop getawaitablefunc and friends in favor of unaryfunc.

--- Objects/genobject.c
+++ Objects/genobject.c
@@ -692,7 +692,7 @@ PyGen_NeedsFinalizing(PyGenObject *gen)
 PyObject *
 _PyGen_GetAwaitableIter(PyObject *o)
 {
-    getawaitablefunc getter = NULL;
+    unaryfunc getter = NULL;
     PyTypeObject *ot;
 
     if (PyGen_CheckCoroutineExact(o)) {
