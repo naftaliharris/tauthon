8b7f38a245c4e1925f92ed3eb3f718613aa30b9d
Naftali Harris <naftaliharris@gmail.com>
2015-11-07 20:37:18 -0800

    matmulType methods ignore types of other objects

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -2359,7 +2359,7 @@ static PyTypeObject matmulType = {
     PyObject_GenericGetAttr,/*tp_getattro*/
     PyObject_GenericSetAttr,/*tp_setattro*/
     0,                      /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_CHECKTYPES, /*tp_flags*/
     0,                      /*tp_doc*/
     0,                      /*tp_traverse*/
     0,                      /*tp_clear*/
