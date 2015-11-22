930f6c2b586d6bc920b74b276c68b59db385cff9
Naftali Harris <naftaliharris@gmail.com>
2015-11-21 18:12:18 -0800

    RSI -> TODO

--- Python/ceval.c
+++ Python/ceval.c
@@ -2120,13 +2120,13 @@ PyEval_EvalFrameEx(PyFrameObject *f, int throwflag)
             if (send == NULL) {
                 send = PyString_InternFromString("send");
                 if (send == NULL) {
-                    return NULL;  /* RSI: handle this error */
+                    return NULL;  /* TODO: is this correct? */
                 }
             }
             if (next == NULL) {
                 next = PyString_InternFromString("next");
                 if (next == NULL) {
-                    return NULL;  /* RSI: handle this error */
+                    return NULL;  /* TODO: is this correct? */
                 }
             }
 
