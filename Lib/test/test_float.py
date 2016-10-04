8f1145046fe5a7bff62858bfaf2aa3c79d212be0
Naftali Harris <naftaliharris@gmail.com>
2016-10-04 11:15:54 -0700

    Added tests for derivative types

--- Lib/test/test_float.py
+++ Lib/test/test_float.py
@@ -9,6 +9,8 @@ import random
 import fractions
 import sys
 import time
+from test.test_grammar import (VALID_UNDERSCORE_LITERALS,
+                               INVALID_UNDERSCORE_LITERALS)
 
 INF = float("inf")
 NAN = float("nan")
@@ -58,6 +60,27 @@ class GeneralFloatCases(unittest.TestCase):
         # (in 2.6, long unicode inputs to float raised ValueError)
         float('.' + '1'*1000)
         float(unicode('.' + '1'*1000))
+
+    def test_underscores(self):
+        for lit in VALID_UNDERSCORE_LITERALS:
+            if not any(ch in lit for ch in 'jJxXoObB'):
+                self.assertEqual(float(lit), eval(lit))
+                self.assertEqual(float(lit), float(lit.replace('_', '')))
+        for lit in INVALID_UNDERSCORE_LITERALS:
+            if lit in ('0_7', '09_99'):  # octals are not recognized here
+                continue
+            if not any(ch in lit for ch in 'jJxXoObB'):
+                self.assertRaises(ValueError, float, lit)
+        # Additional test cases; nan and inf are never valid as literals,
+        # only in the float() constructor, but we don't allow underscores
+        # in or around them.
+        self.assertRaises(ValueError, float, '_NaN')
+        self.assertRaises(ValueError, float, 'Na_N')
+        self.assertRaises(ValueError, float, 'IN_F')
+        self.assertRaises(ValueError, float, '-_INF')
+        self.assertRaises(ValueError, float, '-INF_')
+        # Check that we handle bytes values correctly.
+        self.assertRaises(ValueError, float, b'0_.\xff9')
 
     def test_non_numeric_input_types(self):
         # Test possible non-numeric types for the argument x, including
